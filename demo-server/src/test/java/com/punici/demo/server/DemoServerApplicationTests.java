7d4e1cd17d62d5ee00c5cb1041cb666efd5fe261
pengkailiang <punici@163.com>
2021-12-27 11:43:08 +0800

    调整结构

---
+++ demo-server/src/test/java/com/punici/demo/server/DemoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.punici.demo.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoServerApplicationTests
+{
+    @Test
+    void contextLoads()
+    {
+    }
+}
